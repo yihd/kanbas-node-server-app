dd58d20950a13c5a0e9d0719561ebd84bc5b1518
yihd <huidiw2@uci.edu>
2024-04-16 14:24:20 -0400

    Modify App

--- App.js
+++ App.js
@@ -13,37 +13,32 @@ import Assignments from "./Kanbas/assignments/routes.js";
 import SessionExercises from "./SessionExercises.js";
 import Users from "./users/routes.js";
 
-// Database connection
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-console.log(CONNECTION_STRING);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/Kanbas'
 mongoose.connect(CONNECTION_STRING);
-
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
-// Configure CORS with specific settings
-app.use(cors({
-  credentials: true,
-  origin: process.env.FRONTEND_URL  // Ensure FRONTEND_URL is correctly set in .env
-}));
-
-// Configure session with specific options
 const sessionOptions = {
-  secret: "any string",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {}
 };
-
 if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie.sameSite = "none";
-  sessionOptions.cookie.secure = true;
-  sessionOptions.cookie.domain = process.env.HTTP_SERVER_DOMAIN;
-}
-
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  
 app.use(session(sessionOptions));
+
+app.use(express.json());
 
 // Register routes
 Lab5(app);
@@ -54,8 +49,4 @@ Assignments(app);
 SessionExercises(app);
 Users(app);
 
-// Start server
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(4000 || process.env.PORT);
