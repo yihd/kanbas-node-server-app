775ab4e3022027c8329edca70a7f557fb3dd6e73
yihd <huidiw2@uci.edu>
2024-04-15 22:19:03 -0400

    Modify App

--- App.js
+++ App.js
@@ -20,37 +20,18 @@ console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: process.env.FRONTEND_URL
-//   })
-// );
-const allowedOrigins = [
-  'https://661db0fd653e784754fca682--creative-dragon-e3962c.netlify.app',
-  'https://creative-dragon-e3962c.netlify.app', // Assuming this is your production front-end URL
-  // add any other origins you want to allow requests from
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    // Check if the origin is in the list of allowed origins
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true); // Origin is allowed
-    } else {
-      return callback(new Error('CORS not allowed'), false); // Origin is not allowed
-    }
-  },
-  credentials: true, // Important for cookies, authorization headers with HTTPS
-}));
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
 };
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -62,7 +43,7 @@ if (process.env.NODE_ENV !== "development") {
 
 
 app.use(session(sessionOptions));
-
+app.use(express.json());
 // app.use(
 //   session({
 //     secret: "secret",
