b6818a83c11bdf8967add995f62967b386bc4bf0
yihd <huidiw2@uci.edu>
2024-04-16 00:57:39 -0400

    Modify App

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import mongoose from "mongoose";
 
 
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/Kanbas'
 console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 const app = express();
